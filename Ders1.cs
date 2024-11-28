a70ebb5f2170199ad1955e7a4eece471ca287ec1
YazilimKarargahi <150514251+YazilimKarargahi@users.noreply.github.com>
2024-11-28 22:18:06 +0300

    Create Ders1.cs

---
+++ Ders1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Ders_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba Dunya");
+            Console.ReadLine();
+        }
+
+
+    }
+}
