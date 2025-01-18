75e48c1cd49f1599ebc4daded81dc01b8ad766b9
YazilimKarargahi <150514251+YazilimKarargahi@users.noreply.github.com>
2025-01-19 01:59:33 +0300

    Create Ders5.cs

---
+++ Ders5.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operatorler 
+
+            // Matematiksel Operatorler
+            // + , - , / , * , % , =
+
+
+
+
+
+            // + , = 
+
+             int number1 = 0;
+
+             int number2 = 0;
+
+             int result = 0;
+
+
+
+            //Console.WriteLine(number1 + " artı " + number2 + " esittir " + result);
+
+
+
+
+            // - 
+
+            //Console.Write("Lutfen birinci sayiyi giriniz: ");
+            //number1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Lutfen ikinci sayiyi giriniz: ");
+            //number2 = Convert.ToInt32(Console.ReadLine());
+
+            //result = number1 - number2;
+
+            //Console.WriteLine("Sonuc: " + result);
+
+
+            // /
+
+            //int number3 = 10;
+            //int number4 = 0;
+
+            //int result2 = number3 / number4;
+
+            // Console.WriteLine(result2);
+
+            // * 
+
+            //number1 = 5;
+            //number2 = 10;
+
+            //result = number1 * number2;
+
+            //Console.WriteLine(result);
+
+
+            // % 
+
+            //number1 = 15;
+            //number2 = 5;
+
+            //result = number1 % number2;
+
+            //Console.WriteLine(result);
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
