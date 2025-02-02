be2dd4e1ef137e853e03e19d807f74bef9c6329b
YazilimKarargahi <150514251+YazilimKarargahi@users.noreply.github.com>
2025-02-03 00:16:01 +0300

    Create Ders6.cs

---
+++ Ders6.cs
@@ -0,0 +1,168 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders5Atama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ATAMA OPERATORLERI
+
+            //  = , += , -= , *= , /= , %=  
+
+            //int x = 5;
+
+            //Console.WriteLine("ilk degerimiz : " +x);
+
+            //x = 10;
+
+            //Console.WriteLine("yeni degerimiz : " + x);
+
+
+
+
+
+            // +=
+
+            // x += 1; - > x = x + 1;
+
+
+            //int x = 5;
+
+
+
+            //Console.WriteLine("ilk degerimiz : " + x);
+
+            //x += 1;
+
+            //Console.WriteLine("yeni degerimiz : " + x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // -=
+
+            // x -=1; - > x = x - 1;
+
+
+            //int x = 5;
+
+            //int y = 3;
+
+            //Console.WriteLine("ilk degerimiz : " + x);
+
+            //x -= y;
+
+            //Console.WriteLine("yeni degerimiz : " + x);
+
+
+
+
+
+
+
+
+
+
+
+            // *=
+
+            // x *=1; - > x = x * 1;
+
+
+            //int x = 5;
+
+            //Console.WriteLine("ilk degerimiz : " + x);
+
+            //x *= 2;
+
+            //Console.WriteLine("yeni degerimiz : " + x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // /=
+
+            // x /=1; - > x = x / 1;
+
+
+            //float x = 6;
+
+            //Console.WriteLine("ilk degerimiz : " + x);
+
+            //x /= 4;
+
+            //Console.WriteLine("yeni degerimiz : " + x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // %=1;
+
+            // x +=1; - > x = x % 1;
+
+            int x = 11;
+
+            Console.WriteLine("ilk degerimiz : " + x);
+
+            x %= 2;
+
+            Console.WriteLine("yeni degerimiz : " + x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
