3add3969da5333d067e68d57edc206e90d7844a1
YazilimKarargahi <150514251+YazilimKarargahi@users.noreply.github.com>
2025-02-11 15:54:03 +0300

    Create ders8.cs

---
+++ ders8.cs
@@ -0,0 +1,48 @@
+using System;
+
+
+namespace Ders7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // karsilastirma operatorleri
+
+
+            // == , != , > , < , >= , <=
+
+            // !true = false 
+
+            int x = 3;
+            int y = 3;
+
+           
+
+            Console.Write("karsilastirma sonucu : ");
+
+             Console.WriteLine(x<=y);
+
+
+            //  Console.WriteLine(x >= y);   >=
+
+
+            // Console.WriteLine(x < y); <
+
+            // Console.WriteLine(x > y); >
+
+
+            //    Console.WriteLine(x!=y); !=
+
+            //  Console.WriteLine(x==y); ==
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
